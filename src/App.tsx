cc8a96f05c958b89fe1f533165e940761d3f9595
Corned <Corned@users.noreply.github.com>
2024-07-08 23:16:09 +0300

    terminal

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 function App() {
-
   return (
-    <div className="bg-black w-full h-screen flex flex-col align-center justify-center">
-      <div className="flex flex-col justify-center">
-        <h1 className="text-white text-center select-none" style={{ fontSize: "8rem", textShadow: "rgba(255, 255, 255, 0.2) 0 0 50px" }}>TMP.OOO</h1>
+    <div className="bg-black w-full h-screen flex flex-col items-center justify-center">
 
-        <div className="flex flex-row justify-center gap-3">
-          <button className="px-6 py-2 bg-white/5 text-white rounded-full hover:bg-white hover:text-black select-none">github</button>
-          <button className="px-6 py-2 bg-white/5 text-white rounded-full hover:bg-white hover:text-black select-none">lerp</button>
-          <button className="px-6 py-2 bg-white/5 text-white rounded-full hover:bg-white hover:text-black select-none">navi</button>
-          <button className="px-6 py-2 bg-white/5 text-white rounded-full hover:bg-white hover:text-black select-none">misc</button>
+      <div className="flex flex-col justify-center">
+        <h1
+          className="text-white text-center select-none breathing-text"
+          style={{ fontSize: "8rem", textShadow: "rgba(255, 255, 255, 0.2) 0 0 50px" }}
+        >
+          TMP.OOO
+        </h1>
+      </div>
+
+      <div className="terminal rounded w-[500px] py-2 px-4">
+        <div className="terminal__input">
+          <p> { "> _" }</p>
         </div>
       </div>
+
     </div>
   )
 }
