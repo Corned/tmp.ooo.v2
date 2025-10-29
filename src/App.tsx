27b2790c7d4ae3ef9cd2c00348e837b35de296f6
corned <oliver.kaarnalehto@gmail.com>
2025-10-29 13:49:30 +0200

    basic state of a terminal, input height scales

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,23 @@ import { useEffect, useRef, useState } from 'react'
 function App() {
 
 
+  const [ commandHistory, setCommandHistory ] = useState<string[]>([])
+  const [ currentCommand, setCurrentCommand ] = useState<string>("")
+  const [ historyIndex, setHistoryIndex ] = useState<number>(-1)
+  const inputRef = useRef<HTMLTextAreaElement>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+
+  const handleOnChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setCurrentCommand(event.target.value)
+    // Reset height to auto to calculate new scrollHeight
+    // and Set height to scrollHeight to fit content
+    event.target.style.height = 'auto'
+    event.target.style.height = `${event.target.scrollHeight}px`
+  }
+
   const headerString = "hello world_"
   const spans = headerString.split("").map((char, index) => (
     <span className="breathing-weight" style={{ animationDelay: `${index*(1000/headerString.length)}ms` }} >{ char }</span>
@@ -20,12 +37,16 @@ function App() {
         </h1>
       </div>
 
-      <div className="terminal rounded w-[500px] py-2 px-4">
-        <div className="terminal__input">
-          <p>
-            <span>{"> "}</span>{spans}
-          </p>
-        </div>
+      <div className="terminal rounded w-[500px] py-2 px-4 flex flex-row gap-2 ">
+        <span className="select-none">{"> "}</span>
+        <textarea
+          ref={inputRef}
+          value={currentCommand}
+          onChange={handleOnChange}
+          className="bg-black text-white resize-none flex-1 outline-none border-none overflow-hidden"
+          rows={1}
+          style={{ minHeight: '1.5rem' }}
+        />
       </div>
 
     </div>
