72a4698e3f22480723ba14243aeb38f9571b6c02
Corned <Corned@users.noreply.github.com>
2025-11-18 11:02:55 +0200

    added rpn command

--- src/lib/commands/index.ts
+++ src/lib/commands/index.ts
@@ -1,9 +1,11 @@
 import echo from "./echo";
 import helloworld from "./helloworld";
+import rpn from "./rpn";
 
 const commands = {
   echo,
   helloworld,
+  rpn,
 };
 
 export default commands as Record<string, (...args: string[]) => string>;
---
+++ src/lib/commands/rpn.ts
@@ -0,0 +1,6 @@
+export default function (...args: string[]): string {
+  
+
+
+  return args.join(" ");
+}
