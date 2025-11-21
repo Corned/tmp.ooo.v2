d09848b5f79eb5ef770d18fe3425da52d48989a7
Corned <Corned@users.noreply.github.com>
2025-11-21 09:47:04 +0200

    wip

--- src/lib/commands/rpn.ts
+++ src/lib/commands/rpn.ts
@@ -1,6 +1,18 @@
 export default function (...args: string[]): string {
+  const expression: string = args.join("")
+  const tokens: string[] = []
+
+  // 2 + 4 in RPN is "2 4 +"
+  // 2 + 4 * 3 in RPN is "2 4 3 * +"
+  // 2 + (44-2) in RPN is "2 44 2 - +"
+
+  let currentToken = ""
   
+  for (const c of expression) {
+
+  }
 
 
-  return args.join(" ");
+
+  return tokens.join("")
 }
