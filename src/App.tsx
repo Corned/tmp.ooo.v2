1401886c9212ebd7963ca0dcd3deae8dc92d2062
Corned <Corned@users.noreply.github.com>
2025-11-24 12:55:19 +0200

    testing centered text

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,34 @@ import ITerminalCell from "./types/ITerminalCell"
 
 const COLS = 120
 const ROWS = 32
-
-const str = "const f = (a: number) => a + 1 "
 const block = "█"
 
 const logo = `
- ███████████ ██████   ██████ ███████████        ███████       ███████       ███████
-▒█▒▒▒███▒▒▒█▒▒██████ ██████ ▒▒███▒▒▒▒▒███     ███▒▒▒▒▒███   ███▒▒▒▒▒███   ███▒▒▒▒▒███
-▒   ▒███  ▒  ▒███▒█████▒███  ▒███    ▒███    ███     ▒▒███ ███     ▒▒███ ███     ▒▒███
-    ▒███     ▒███▒▒███ ▒███  ▒██████████    ▒███      ▒███▒███      ▒███▒███      ▒███
-    ▒███     ▒███ ▒▒▒  ▒███  ▒███▒▒▒▒▒▒     ▒███      ▒███▒███      ▒███▒███      ▒███
-    ▒███     ▒███      ▒███  ▒███           ▒▒███     ███ ▒▒███     ███ ▒▒███     ███
-    █████    █████     █████ █████        ██ ▒▒▒███████▒   ▒▒▒███████▒   ▒▒▒███████▒
-   ▒▒▒▒▒    ▒▒▒▒▒     ▒▒▒▒▒ ▒▒▒▒▒        ▒▒    ▒▒▒▒▒▒▒       ▒▒▒▒▒▒▒       ▒▒▒▒▒▒▒
+  ███████████ ██████   ██████ ███████████        ███████       ███████       ███████   
+ ▒█▒▒▒███▒▒▒█▒▒██████ ██████ ▒▒███▒▒▒▒▒███     ███▒▒▒▒▒███   ███▒▒▒▒▒███   ███▒▒▒▒▒███ 
+ ▒   ▒███  ▒  ▒███▒█████▒███  ▒███    ▒███    ███     ▒▒███ ███     ▒▒███ ███     ▒▒███
+     ▒███     ▒███▒▒███ ▒███  ▒██████████    ▒███      ▒███▒███      ▒███▒███      ▒███
+     ▒███     ▒███ ▒▒▒  ▒███  ▒███▒▒▒▒▒▒     ▒███      ▒███▒███      ▒███▒███      ▒███
+     ▒███     ▒███      ▒███  ▒███           ▒▒███     ███ ▒▒███     ███ ▒▒███     ███ 
+    █████    █████     █████ █████        ██ ▒▒▒███████▒   ▒▒▒███████▒   ▒▒▒███████▒   
+   ▒▒▒▒▒    ▒▒▒▒▒     ▒▒▒▒▒ ▒▒▒▒▒        ▒▒    ▒▒▒▒▒▒▒       ▒▒▒▒▒▒▒       ▒▒▒▒▒▒▒     
 `
 
 function App() {
   const [ state, setState, write, writeString ] = Terminal(COLS, ROWS)
 
-
-
   useEffect(() => {
     let stateCopy = { ...state }
-    stateCopy = writeString(stateCopy, logo)
+    for (const line of logo.split("\n")) {
+      stateCopy.cursorCol = Math.floor((COLS - line.length) / 2)
+      stateCopy = writeString(stateCopy, line)
+      stateCopy = writeString(stateCopy, "\n")
+    }
     stateCopy = writeString(stateCopy, "\n")
-    stateCopy = writeString(stateCopy, "Hello, World! Welcome to tmp.ooo!")
-    stateCopy = writeString(stateCopy, "\n")
-    stateCopy = writeString(stateCopy, str)
-    stateCopy = writeString(stateCopy, "\n")
+    const line = "Welcome to tmp.ooo!\n\n\n"
+    stateCopy.cursorCol = Math.floor((COLS - line.length) / 2)
+    stateCopy = writeString(stateCopy, line)
+    stateCopy = writeString(stateCopy, "\n\ntmp.ooo $ ")
     setState(() => stateCopy)
 
   }, [])
@@ -42,20 +42,20 @@ function App() {
       style={{ backgroundColor: "#16161d" }}
     >
       <div
-        className="terminal grid"
+        className="terminal grid border border-gray-700 p-1"
         style={{
           display: "grid",
           gridTemplateColumns: `repeat(${COLS}, 1fr)`,
           gridTemplateRows: `repeat(${ROWS}, 1fr)`,
           width: "fit-content",
-          gap: 0,
+          gap: 1,
         }}
       >
         {state.grid.map((row: ITerminalCell[], rowIndex: number) =>
           row.map((cell: ITerminalCell, colIndex: number) => (
             <span
               key={`cell-${rowIndex}-${colIndex}`}
-              className={`text-lg leading-none text-white ${cell.bold ? "font-bold" : ""} ${cell.italic ? "italic" : ""} ${cell.underline ? "underline" : ""}`}
+              className={`text-lg leading-none text-white  ${cell.bold ? "font-bold" : ""} ${cell.italic ? "italic" : ""} ${cell.underline ? "underline" : ""}`}
               style={{ color: cell.color, backgroundColor: cell.bgColor }}
             >
               {
